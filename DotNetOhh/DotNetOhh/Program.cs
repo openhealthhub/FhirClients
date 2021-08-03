6d195e3f78760693ced839ab081b4f5e52a60a51
Silvan van Leeuwen <silvan@openhealthhub.com>
2021-08-03 11:10:37 +0200

    OP-651: add example for questionnaire

---
+++ DotNetOhh/DotNetOhh/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Hl7.Fhir.Model;
+
+namespace DotNetOhh
+{
+    using Hl7.Fhir.Rest;
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var settings = new FhirClientSettings
+            {
+                PreferredFormat = ResourceFormat.Json,
+                VerifyFhirVersion = true
+            };
+
+            var client = new FhirClient("https://api-sandbox-staging.openhealthhub.com/fhir/", settings);
+            var questionnaire = client.Read<Questionnaire>("Questionnaire/1");
+            
+            Console.Out.WriteLine(questionnaire.Description);
+        }
+    }
+}
