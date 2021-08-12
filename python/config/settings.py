c172170f3a29596ae8480a3a320866588663e14d
Silvan van Leeuwen <silvan@openhealthhub.com>
2021-08-12 15:41:07 +0200

    OP-651: check encryption status similarly to other languages

--- python/config/settings.py
+++ python/config/settings.py
@@ -1,7 +1,8 @@
 from fhirclient import client
+from fhirclient.server import FHIRServer
 
 cli = client.FHIRClient(settings={
     'app_id': 'ohh-fhir',
-    'api_base': 'https://api-sandbox-staging.openhealthhub.com/fhir'
+    'api_base': 'http://localhost:8080/fhir' #'https://api-sandbox-staging.openhealthhub.com/OpenHealthhub/fhir/4'
 })
-server = cli.server
+server: FHIRServer = cli.server
