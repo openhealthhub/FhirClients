3575dc6a2c95d60d010f9702892c1597400765e4
Eddy Bakker <eddy@openhealthhub.com>
2021-08-13 09:20:41 +0200

    OP-685 add api key to ruby requests

--- ruby/src/fhir_client.rb
+++ ruby/src/fhir_client.rb
@@ -4,6 +4,7 @@ require 'fhir_client'
 class FhirClient
   def initialize
     client = FHIR::Client.new('https://api-sandbox-staging.openhealthhub.com/OpenHealthhub/fhir/4')
+    client.additional_headers = {'x-api-key': 'ad880601-b7e6-4d86-901d-b6fca96fc725'}
     FHIR::Model.client = client
     # Workaround for https://github.com/fhir-crucible/fhir_models/issues/93
     client.default_xml
