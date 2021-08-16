944efdbb141cc4b5b9681d2f2a7ef95dd28935b5
Eddy Bakker <eddy@openhealthhub.com>
2021-08-16 16:15:02 +0200

    OP-685 add js example of keycloak auth

--- javascript/js/constants.js
+++ javascript/js/constants.js
@@ -1,2 +1,8 @@
-export const FHIR_ENDPOINT = 'http://localhost:8080/eddy/fhir/2.0';
-export const API_KEY = 'ad880601-b7e6-4d86-901d-b6fca96fc725';
+export const FHIR_ENDPOINT = 'https://api-sandbox-staging.openhealthhub.com/OpenHealthhub/fhir/4';
+export const API_KEY = 'f6f5ab9e-c131-4b7e-a38b-9053cd621cf4';
+
+const KEYCLOAK_BASE_URL = 'https://auth-staging.openhealthhub.com/auth';
+const REALM = 'OpenHealthHub';
+export const KEYCLOAK_TOKEN_URI = `${KEYCLOAK_BASE_URL}/realms/${REALM}/protocol/openid-connect/token`;
+export const CLIENT_SECRET = '915e87d4-16ee-4ca5-b701-b38b6afce8ff';
+export const CLIENT_ID = 'api-sandbox';
