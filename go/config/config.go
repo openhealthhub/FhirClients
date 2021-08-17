14445e65078b0b949269093f3df4c5edca050cb9
Eddy Bakker <eddy@openhealthhub.com>
2021-08-17 11:07:12 +0200

    OP-685 add go example of keycloak auth

--- go/config/config.go
+++ go/config/config.go
@@ -3,5 +3,10 @@ package config
 import "github.com/google/fhir/go/fhirversion"
 
 const Api = "https://api-sandbox-staging.openhealthhub.com/OpenHealthhub/fhir/4"
+const ApiKey = "90da723b-70f7-400b-8ddb-8574c45bff13"
 const TimeZone = "Europe/Amsterdam"
 const FhirVersion = fhirversion.R4
+
+const KeycloakTokenUri = "https://auth-staging.openhealthhub.com/auth/realms/OpenHealthHub/protocol/openid-connect/token"
+const KeycloakClientId = "api-sandbox"
+const KeycloakClientSecret = "915e87d4-16ee-4ca5-b701-b38b6afce8ff"
