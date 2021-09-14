e085ffeffc0afee7f43d8d6052d8dc14650bb282
Silvan van Leeuwen <silvan@openhealthhub.com>
2021-09-14 11:21:06 +0200

    OP-709: update examples

--- php/src/OpenHealthhub/PhpFhirClient/SubscriptionClient.php
+++ php/src/OpenHealthhub/PhpFhirClient/SubscriptionClient.php
@@ -5,6 +5,7 @@ namespace OpenHealthhub\PhpFhirClient;
 
 use DCarbone\PHPFHIRGenerated\R4\FHIRElement\FHIRBackboneElement\FHIRSubscription\FHIRSubscriptionChannel;
 use DCarbone\PHPFHIRGenerated\R4\FHIRElement\FHIRSubscriptionChannelType;
+use DCarbone\PHPFHIRGenerated\R4\FHIRElement\FHIRSubscriptionStatus;
 use DCarbone\PHPFHIRGenerated\R4\FHIRResource\FHIRDomainResource\FHIRSubscription;
 
 class SubscriptionClient
@@ -16,6 +17,8 @@ class SubscriptionClient
         $channel = new FHIRSubscriptionChannel();
         $channelType = new FHIRSubscriptionChannelType();
         $subscription->setCriteria('Appointment?name=test')->setChannel($channel->setEndpoint('https://your-webhook/endpoint')->setType($channelType->setValue('rest-hook')));
+        $status = new FHIRSubscriptionStatus(['requested']);
+        $subscription->setStatus($status);
         $res = $client->create('Subscription', $subscription);
         return new FHIRSubscription($res);
     }
