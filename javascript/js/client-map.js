313750b6fbf64fb0a6f75adfd3bc3d6fea2b6558
Silvan van Leeuwen <silvan@openhealthhub.com>
2021-09-17 12:18:22 +0200

    OP-820: Add javascript careplan client

--- javascript/js/client-map.js
+++ javascript/js/client-map.js
@@ -3,11 +3,13 @@ import QuestionnaireClient from "./questionnaire";
 import SubscriptionClient from "./subscription";
 import QuestionnaireResponseClient from './questionnaire-response';
 import VitalSignsClient from "./vitalsigns";
+import CarePlanClient from "./careplan";
 
 export const CLIENT_MAP = {
   Appointment: new AppointmentClient(),
   Questionnaire: new QuestionnaireClient(),
   Subscription: new SubscriptionClient(),
   QuestionnaireResponse: new QuestionnaireResponseClient(),
-  VitalSigns: new VitalSignsClient()
+  VitalSigns: new VitalSignsClient(),
+  CarePlan: new CarePlanClient()
 }
