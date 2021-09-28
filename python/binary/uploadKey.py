e101d94e59b923ebcf94d07e4e218820ea2f1118
Eddy Bakker <eddy@openhealthhub.com>
2021-09-28 10:23:13 +0200

    Update python/binary/uploadKey.py
    Co-authored-by: Silvan van Leeuwen <silvansmeel@gmail.com>

--- python/binary/uploadKey.py
+++ python/binary/uploadKey.py
@@ -6,7 +6,7 @@ import requests
 from config.settings import FHIR_URL, get_token, API_KEY
 
 
-async def get_observation():
+async def create_key():
     encoded_key = await get_base64_encoded_key()
     token = get_token()
     resp = requests.post(url=FHIR_URL + '/Binary',
